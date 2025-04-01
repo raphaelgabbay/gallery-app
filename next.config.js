a20e347b5df07ac01b01adb21c291361f8cdc88a
Raphael <raphael.gabbay@gmail.com>
2025-04-01 23:49:11 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/gallery-app',
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: '/gallery-app/',
+  trailingSlash: true,
+  distDir: 'out'
 }
 
 module.exports = nextConfig 
