5d50f158a3105fb0e498dad3958eadad6f75bdcd
Raphael <raphael.gabbay@gmail.com>
2025-04-01 23:08:46 +0200

    Change Author of placeholder component

--- app/components/gallery/componentRegistry.js
+++ app/components/gallery/componentRegistry.js
@@ -1,13 +1,10 @@
-// This file will be updated automatically with components from students
-// Example structure
-
-import PlaceholderComponent from '../../components/shared/PlaceholderComponent';
+// Fichier généré automatiquement - NE PAS MODIFIER DIRECTEMENT
+import PlaceholderComponent from '../shared/PlaceholderComponent';
 
 export const componentRegistry = {
   "PlaceholderComponent": {
     Component: PlaceholderComponent,
-    author: "Formateur",
+    author: "Raphael",
     description: "Un composant d'exemple en attendant les contributions des élèves."
   },
-  // Other components will be added here automatically
-}; 
+};
