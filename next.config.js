9056f303fe6d3edd308301dd210d92988a3b26ac
Raphael <raphael.gabbay@gmail.com>
2025-04-02 13:07:19 +0200

    Update webpack configuration for proper chunk generation

--- next.config.js
+++ next.config.js
@@ -11,8 +11,28 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.output.publicPath = '/gallery-app/';
-      delete config.output.chunkFilename;
-      delete config.output.filename;
+      config.output.chunkFilename = 'static/chunks/[name].[contenthash].js';
+      config.output.filename = 'static/chunks/[name].[contenthash].js';
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 244000,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      };
     }
     return config;
   }
