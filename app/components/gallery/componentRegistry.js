b5674d1baf141026791deed40257f4ffbc3e1684
Sylvain <40465096+sylvainferrer@users.noreply.github.com>
2025-04-02 15:32:16 +0200

    form

--- app/components/gallery/componentRegistry.js
+++ app/components/gallery/componentRegistry.js
@@ -7,6 +7,11 @@ export const componentRegistry = {
     author: "Formateur",
     description: "Un composant d'exemple en attendant les contributions des élèves."
   },
+  "Sylvain-FormulaireDeContact": {
+    Component: require('../teams/Sylvain/FormulaireDeContact.jsx').default,
+    author: "Équipe Sylvain",
+    description: "Composant créé par l'équipe Sylvain"
+  },
   "team1-FancyButton": {
     Component: require('../teams/team1/FancyButton.js').default,
     author: "Team 1 - Student Demo",
