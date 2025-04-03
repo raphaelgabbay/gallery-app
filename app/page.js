db863c8ec260931b0cebeb82d4d7ce918659098c
Raphael Gabbay <44260702+raphaelgabbay@users.noreply.github.com>
2025-04-03 13:29:56 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
 
-import ComponentGallery from './components/gallery/ComponentGallery'
-
 import fs from 'fs'
 import path from 'path'
 import ComponentGallery from './components/gallery/ComponentGallery'
